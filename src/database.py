c5f396b6c92f5d404bc6a35e9e54f1a71d4fd721
jwu205 <jinwu2003@gmail.com>
2023-11-05 23:57:07 -0800

    stores update endpoints

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import os
 import dotenv
+import sqlalchemy
 from sqlalchemy import create_engine
 
 def database_connection_url():
@@ -8,3 +9,6 @@ def database_connection_url():
     return os.environ.get("POSTGRES_URI")
 
 engine = create_engine(database_connection_url(), pool_pre_ping=True)
+
+metadata_obj = sqlalchemy.MetaData()
+stores = sqlalchemy.Table("stores", metadata_obj, autoload_with=engine)
