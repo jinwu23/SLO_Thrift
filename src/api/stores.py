48163b1e93c5bb1cb03fa05e985b92a89e319e0d
unknown <jinwu2003@gmail.com>
2023-12-03 22:57:33 -0800

    performance testing queries

--- src/api/stores.py
+++ src/api/stores.py
@@ -4,6 +4,7 @@ from src.api import auth
 import math
 import sqlalchemy
 from src import database as db
+from sqlalchemy import text
 
 router = APIRouter(
     prefix="/stores",
@@ -22,27 +23,52 @@ def get_stores():
     Retrieves the catalog of thrift stores in website.
     """
     stores_arr = []
+
     with db.engine.begin() as connection:
+        
+        # # performance testing query
+        # performance_results = connection.execute(text(
+        #         """
+        #         EXPLAIN ANALYZE
+        #         SELECT 
+        #             stores.id as id, 
+        #             stores.name as name, 
+        #             stores.address as address, 
+        #             stores.type as type, 
+        #             AVG(reviews.rating) as rating 
+        #         FROM stores 
+        #         LEFT JOIN reviews on reviews.store_id = stores.id 
+        #         GROUP BY stores.id
+        #         ORDER BY stores.id asc
+        #         """))
+        # query_plan = performance_results.fetchall()
+        # for row in query_plan:
+        #     print(row)
+
         results = connection.execute(
             sqlalchemy.text(
                 """
-                SELECT stores.id, stores.name, 
-                stores.address, stores.type, AVG(reviews.rating) 
-                as rating FROM stores 
+                SELECT 
+                    stores.id as id, 
+                    stores.name as name, 
+                    stores.address as address, 
+                    stores.type as type, 
+                    AVG(reviews.rating) as rating 
+                FROM stores 
                 LEFT JOIN reviews on reviews.store_id = stores.id 
                 GROUP BY stores.id
+                ORDER BY stores.id asc
                 """))
         results = results.fetchall()
-        stores_arr = [
+        for row in results:
+            stores_arr.append(
                 {   
                     "id": row.id,
                     "name": row.name,
                     "rating": row.rating,
                     "address": row.address,
                     "type": row.type,
-                } 
-                for row in results
-                ]
+                })
     print("get_stores")
     return stores_arr
 
@@ -52,14 +78,32 @@ def get_specific_store(id: int):
     Get a specific thrift store given id
     """
     with db.engine.begin() as connection:
+        # # performance testing query
+        # performance_results = connection.execute(text(
+        #         """
+        #         EXPLAIN ANALYZE
+        #         SELECT stores.id, stores.name, 
+        #         stores.address, stores.type, AVG(reviews.rating)
+        #         as rating FROM stores 
+        #         LEFT JOIN reviews on reviews.store_id = stores.id 
+        #         WHERE stores.id = :id 
+        #         GROUP BY stores.id
+        #         """), 
+        #         {"id": id})
+        # query_plan = performance_results.fetchall()
+        # for row in query_plan:
+        #     print(row)
+
         results = connection.execute(
             sqlalchemy.text(
-                """SELECT stores.id, stores.name, 
+                """
+                SELECT stores.id, stores.name, 
                 stores.address, stores.type, AVG(reviews.rating)
                 as rating FROM stores 
                 LEFT JOIN reviews on reviews.store_id = stores.id 
                 WHERE stores.id = :id 
-                GROUP BY stores.id"""), 
+                GROUP BY stores.id
+                """), 
                 {"id": id})
         row = results.fetchone()
         if row:
@@ -80,6 +124,24 @@ def create_store(new_store: Store):
     Creates new thrift stores in website.
     """
     with db.engine.begin() as connection:
+
+        # # performance testing query
+        # performance_results = connection.execute(text(
+        #         """
+        #         BEGIN;
+        #         EXPLAIN ANALYZE
+        #         INSERT INTO stores
+        #         (name, address, type)
+        #         VALUES(:name, :address, :type) 
+        #         RETURNING id;
+        #         """), 
+        #         [{"name": new_store.name, 
+        #           "address": new_store.address, 
+        #           "type": new_store.type}])
+        # query_plan = performance_results.fetchall()
+        # for row in query_plan:
+        #     print(row)
+
         result = connection.execute(
             sqlalchemy.text(
                 """
@@ -137,6 +199,20 @@ def update_store(store_id: int, attribute: str, new_attribute: str):
                 SET type = :new_attribute
                 where id = :id
                 """
+    # # performance testing query
+    # with db.engine.begin() as connection:
+    #     performance_results = connection.execute(text(
+    #         """ 
+    #             EXPLAIN ANALYZE
+    #             UPDATE stores
+    #             SET type = :new_attribute
+    #             where id = :id
+    #             """
+    #     ), [{"id": store_id, "attribute": attribute, "new_attribute": new_attribute}])
+    #     query_plan = performance_results.fetchall()
+    #     for row in query_plan:
+    #         print(row)
+
     with db.engine.begin() as connection:
         connection.execute(
             sqlalchemy.text(query),
